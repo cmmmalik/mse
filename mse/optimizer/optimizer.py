a9c680ee5144f7dda6f60f583fca4199785b328e
cmmmalik <malik@mm.tu-darmstadt.de>
2025-06-17 12:18:32 +0200

    allow for converting atoms with FixSymmetry todict

--- mse/optimizer/optimizer.py
+++ mse/optimizer/optimizer.py
@@ -306,7 +306,9 @@ class Moptimize:
                             new_dct = {"name": c.__class__.__name__, # that's whi ase knows which class to import
                                         }
                             new_dct.update(c.__dict__)
-                            const_lst.append(new_dct)
+                        if new_dct["name"] == "FixSymmetry": # at the moment  atoms are also inserted in the dictionary..
+                            new_dct["kwargs"].pop("atoms")
+                        const_lst.append(new_dct)
 
                     v["constraints"] = const_lst
             dct[k] = v
